f9c2348f4f896c4658e01a71814620f20c772493
zxccanina <nikiforovtimofej965@gmail.com>
2026-03-04 14:45:34 +0300

    Added basic backend program

---
+++ backend/program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Никифоров Тимофей Андреевич");
+        Console.WriteLine(DateTime.Now);
+    }
+}
+
